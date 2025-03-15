27cb38fd31bdee72572238b1a4e657df5fd25528
Lakshmi P G <102515829+melpg@users.noreply.github.com>
2025-03-15 23:12:22 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -32,15 +32,15 @@ ALLOWED_HOSTS = [
     'localhost',
     'https://lakshmipg-8000.theianext-0-labs-prod-misc-tools-'
     'us-east-0.proxy.cognitiveclass.ai',
-    'https://lakshmipg-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01\
-    .proxy.cognitiveclass.ai'
+    'https://lakshmipg-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01'
+    '.proxy.cognitiveclass.ai'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://lakshmipg-8000.theianext-0-labs-prod-misc-tools-'
     'us-east-0.proxy.cognitiveclass.ai',
-    'https://lakshmipg-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01\
-    .proxy.cognitiveclass.ai'
+    'https://lakshmipg-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01'
+    '.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
