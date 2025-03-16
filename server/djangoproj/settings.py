c3bccba673643676d56941ad4d817213f726d8ef
melpg <lakshmi.pg@outlook.com>
2025-03-16 14:57:37 -0400

    deploy

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -147,6 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
