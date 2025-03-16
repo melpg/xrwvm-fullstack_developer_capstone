449a88b9cb18fc54b354bb192164cab7cb1fd5f0
melpg <lakshmi.pg@outlook.com>
2025-03-16 14:14:51 -0400

    deploy

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
@@ -35,5 +35,4 @@ urlpatterns = [
     path(
         'postreview/<int:dealer_id>',
         TemplateView.as_view(template_name="index.html")),
-    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
