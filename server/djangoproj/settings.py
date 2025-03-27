bd976b91dadae1e675515939495ba0ff1ac865f2
melpg <lakshmi.pg@outlook.com>
2025-03-27 16:31:19 -0400

    S

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -100,21 +100,12 @@ WSGI_APPLICATION = 'djangoproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if os.getenv("RENDER"):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': '/var/lib/sqlite/db.sqlite3',  # For Render
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
