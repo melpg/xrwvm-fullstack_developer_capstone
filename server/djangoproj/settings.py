aee435374e56ccdf6d05ead99fde11b89f0013db
melpg <lakshmi.pg@outlook.com>
2025-03-27 12:11:25 -0400

    GSPS

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'djangoproj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/var/lib/sqlite/db.sqlite3',
     }
 }
 
