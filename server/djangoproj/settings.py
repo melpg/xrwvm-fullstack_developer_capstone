ae237011a1730112473e1ebebe41f13e061c4a11
melpg <lakshmi.pg@outlook.com>
2025-03-28 15:01:51 -0400

    docker

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY =\
     'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ["*",
