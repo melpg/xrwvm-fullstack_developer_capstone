9dc20febdde0fb26f7a50646e176325d8eac432a
Lakshmi P G <102515829+melpg@users.noreply.github.com>
2025-03-29 20:53:56 +0530

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
