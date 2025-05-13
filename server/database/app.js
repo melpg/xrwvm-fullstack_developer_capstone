bc9484daf909de12d4a8390bc53e75e684f8b84a
Lakshmi P G <102515829+melpg@users.noreply.github.com>
2025-05-13 20:21:51 +0530

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -12,7 +12,9 @@ app.use(require('body-parser').urlencoded({ extended: false }));
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
 const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
 
-mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
+//mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 
 const Reviews = require('./review');
